c5b166166006def5d55bf6856c643d3e83fc7e9d
c <JCarlos06@gmail.com>
2023-10-06 04:10:48 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementById("estadoSelect").addEventListener("change", function() {
+//     const selectedValue = this.options[this.selectedIndex].value;
+    
+//     // Elimina clases previas
+//     this.classList.remove("pagado", "faltante", "desistio");
+
+//     // Añade la clase basada en el valor seleccionado
+//     this.classList.add(selectedValue);
+// });
